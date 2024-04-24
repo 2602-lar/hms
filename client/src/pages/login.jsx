6be02bea1996e423f25d07c50f9c807a964d34e5
Lazie Maphion <79660134+2602-lar@users.noreply.github.com>
2024-04-24 10:38:25 +0200

    login page

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div className='h-screen w-screen'>
+        <img src={'tinydocs.jpg' } alt="" className='w-full h-full'/>
+   
+    <div className='fixed z-10 backdrop-blur-sm h-[60%] w-[40%] top-[20%] left-[30%]'></div>
+    </div>
+  )
+}
